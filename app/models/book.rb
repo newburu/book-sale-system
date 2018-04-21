4de7f657783943d2af2fcd5bde20cebbc6212a4c
Newburu <newburu@gmail.com>
2018-04-21 23:02:47 +0900

    書籍情報更新にトランザクションを導入

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,28 +2,32 @@ class Book < ApplicationRecord
   belongs_to :writer
 
   def self.update_books
-    Book.delete_all
-    user_writers = UserWriter.all.pluck(:writer_id).uniq
-    con = {id_in: user_writers}
-    writers = Writer.ransack(con).result
-    writers.each do |writer|
-      books = []
-      amazon = Amazon::Ecs.item_search(writer.name, :response_group => 'Images,ItemAttributes,OfferSummary', :search_index => 'Books', :country => 'jp')
-      amazon.items.each do |item|
-        book = Book.new
-        book.name = item.get("ItemAttributes/Title")  # 商品タイトル
-        book.sale_date = item.get("ItemAttributes/PublicationDate")  # 発売日
-        book.money = item.get("OfferSummary/LowestNewPrice/Amount")  # 定価
-        book.isbn = item.get("ItemAttributes/ISBN")  # ISBN
-        book.url = item.get("DetailPageURL")  # 詳細ページURL
-        book.image_url = item.get("SmallImage/URL")  # 画像URL
-        book.writer = writer
-        
-        # ISBNが無い場合は、セット販売等になるため、登録しない
-        books << book if book.isbn.present?
+    Book.transaction do
+      Book.delete_all
+      user_writers = UserWriter.all.pluck(:writer_id).uniq
+      con = {id_in: user_writers}
+      writers = Writer.ransack(con).result
+      writers.each do |writer|
+        books = []
+        amazon = Amazon::Ecs.item_search(writer.name, :response_group => 'Images,ItemAttributes,OfferSummary', :search_index => 'Books', :country => 'jp')
+        amazon.items.each do |item|
+          book = Book.new
+          book.name = item.get("ItemAttributes/Title")  # 商品タイトル
+          book.sale_date = item.get("ItemAttributes/PublicationDate")  # 発売日
+          book.money = item.get("OfferSummary/LowestNewPrice/Amount")  # 定価
+          book.isbn = item.get("ItemAttributes/ISBN")  # ISBN
+          book.url = item.get("DetailPageURL")  # 詳細ページURL
+          book.image_url = item.get("SmallImage/URL")  # 画像URL
+          book.writer = writer
+          
+          # ISBNが無い場合は、セット販売等になるため、登録しない
+          books << book if book.isbn.present?
+        end
+        Book.import books
       end
-      Book.import books
     end
+  rescue => e
+    p e.message
   end
 
 end
